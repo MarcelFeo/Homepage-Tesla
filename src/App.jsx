25a36d014d73e353b542ca52dd331d8f1b077a69
Marcel Féo <69916500+MarcelFeo@users.noreply.github.com>
2021-10-30 14:33:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Page from './components/Page'
+
+import { GlobalStyles }  from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <div>
+      <Page />
+      <GlobalStyles />
+    </div>
+  );
+}
+
+export default App;
